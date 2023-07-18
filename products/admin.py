d686f636f483fa150ef0a51e748938548c8f7bea
zbari023 <barry450643@gmail.com>
2023-07-18 21:53:44 +0200

    add admin costumization

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product , Menu
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','menu']
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Menu)
